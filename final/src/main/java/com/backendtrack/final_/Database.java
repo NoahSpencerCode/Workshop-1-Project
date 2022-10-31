d5c4f5d6ac354ab0044828c464b7dbadb7e647b1
NoahSpencerCode <noah@BigMac.askey.com>
2022-10-31 10:10:00 -0700

    Organizations + JWT fix

--- final/src/main/java/com/backendtrack/final_/Database.java
+++ final/src/main/java/com/backendtrack/final_/Database.java
@@ -1,21 +1,16 @@
 package com.backendtrack.final_;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
+import io.vertx.core.*;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.JWTOptions;
 import io.vertx.ext.auth.KeyStoreOptions;
-import io.vertx.ext.auth.PubSecKeyOptions;
-import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.ext.auth.mongo.*;
 import io.vertx.ext.mongo.MongoClient;
 
-import java.lang.reflect.Array;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.List;
 
 
 public class Database extends AbstractVerticle {
@@ -84,6 +79,20 @@ public class Database extends AbstractVerticle {
       startPromise.complete();
     }
 
+    private void createUser(JsonObject user, Message<JsonObject> message) {
+
+      userUtil.createUser(user.getString("username"),user.getString("password"), stringAsyncResult -> {
+
+        if (stringAsyncResult.succeeded()) {
+          message.reply("Successfully registered");
+        } else {
+          message.fail(2, "insert failed: " + stringAsyncResult.cause().getMessage());
+        }
+
+      });
+
+    }
+
     private void registerUser(Message<JsonObject> message) {
       JsonObject user = message.body().getJsonObject("user");
 
@@ -101,19 +110,31 @@ public class Database extends AbstractVerticle {
 
             System.out.println("User not registered");
 
-            userUtil.createUser(user.getString("username"),user.getString("password"), stringAsyncResult -> {
+            createUser(user, message);
 
-              if (stringAsyncResult.succeeded()) {
-                message.reply("Successfully registered");
-              } else {
-                message.fail(2, "insert failed: " + stringAsyncResult.cause().getMessage());
-              }
-
-            });
           }
         } else {
           listAsyncResult.cause().printStackTrace();
           message.fail(2, "find failed: " + listAsyncResult.cause().getMessage());
+        }
+      });
+
+    }
+
+    private void authenticateLogin(JsonObject user, Message<JsonObject> message) {
+
+      JsonObject authInfo = new JsonObject()
+        .put("username", user.getString("username"))
+        .put("password", user.getString("password"));
+
+      authentication.authenticate(authInfo, userAsyncResult -> {
+        if (userAsyncResult.succeeded()) {
+
+          message.reply(true);
+
+        } else {
+          userAsyncResult.cause().printStackTrace();
+          message.fail(1, "login failed: " + userAsyncResult.cause().getMessage());
         }
       });
 
@@ -128,20 +149,7 @@ public class Database extends AbstractVerticle {
         if (listAsyncResult.succeeded()) {
           if (!listAsyncResult.result().isEmpty()) {
 
-            JsonObject authInfo = new JsonObject()
-              .put("username", user.getString("username"))
-              .put("password", user.getString("password"));
-
-            authentication.authenticate(authInfo, userAsyncResult -> {
-              if (userAsyncResult.succeeded()) {
-
-                message.reply(true);
-
-              } else {
-                userAsyncResult.cause().printStackTrace();
-                message.fail(1, "login failed: " + userAsyncResult.cause().getMessage());
-              }
-            });
+            authenticateLogin(user, message);
 
           } else {
             message.fail(1, "User not found");
@@ -155,6 +163,55 @@ public class Database extends AbstractVerticle {
 
     }
 
+    private void createCard(JsonObject user, String[] card, Message<JsonObject> message, AsyncResult<Message<Object>> messageAsyncResult) {
+
+      cardUtil.createUser(user.getString("username"),card[0], stringAsyncResult -> {
+
+        if (stringAsyncResult.succeeded()) {
+
+          JsonObject query = new JsonObject()
+            .put("username", user.getString("username"));
+
+          JsonObject update = new JsonObject().put("$set", new JsonObject()
+            .put("credit-limit", card[1])
+            .put("balance", "0"));
+
+          client.updateCollection("card", query, update, res -> {
+            if (res.succeeded()) {
+              message.reply(messageAsyncResult.result().body().toString());
+            } else {
+              message.fail(1, "update failed: " + res.cause().getMessage());
+            }
+
+          });
+
+        } else {
+          message.fail(2, "insert failed: " + stringAsyncResult.cause().getMessage());
+        }
+
+      });
+
+    }
+
+    private void requestCredit(JsonObject user, Message<JsonObject> message) {
+
+      JsonObject bus_Msg = new JsonObject()
+        .put("credit-score", user.getString("credit-score"));
+
+      bussin.request("credit",bus_Msg, messageAsyncResult -> {
+        if (messageAsyncResult.succeeded()) {
+
+          String[] card = messageAsyncResult.result().body().toString().split(" ");
+
+          createCard(user, card, message, messageAsyncResult);
+
+        } else {
+          messageAsyncResult.cause().printStackTrace();
+          message.fail(1, "apply card failed: " + messageAsyncResult.cause().getMessage());
+        }
+      });
+
+    }
     private void applyCard(Message<JsonObject> message) {
 
       JsonObject user = message.body().getJsonObject("user");
@@ -165,50 +222,61 @@ public class Database extends AbstractVerticle {
             message.fail(1, "card already exists for user");
           } else {
 
-            JsonObject bus_Msg = new JsonObject()
-              .put("credit-score", user.getString("credit-score"));
-
-            bussin.request("credit",bus_Msg, messageAsyncResult -> {
-              if (messageAsyncResult.succeeded()) {
-
-                String[] card = messageAsyncResult.result().body().toString().split(" ");
-
-                cardUtil.createUser(user.getString("username"),card[0], stringAsyncResult -> {
-
-                  if (stringAsyncResult.succeeded()) {
-
-                    JsonObject query = new JsonObject()
-                      .put("username", user.getString("username"));
-
-                    JsonObject update = new JsonObject().put("$set", new JsonObject()
-                      .put("credit-limit", card[1])
-                      .put("balance", "0"));
-
-                    client.updateCollection("card", query, update, res -> {
-                      if (res.succeeded()) {
-                        message.reply(messageAsyncResult.result().body().toString());
-                      } else {
-                        message.fail(1, "update failed: " + res.cause().getMessage());
-                      }
-
-                    });
-
-                  } else {
-                    message.fail(2, "insert failed: " + stringAsyncResult.cause().getMessage());
-                  }
-
-                });
-
-              } else {
-                messageAsyncResult.cause().printStackTrace();
-                message.fail(1, "apply card failed: " + messageAsyncResult.cause().getMessage());
-              }
-            });
+            requestCredit(user, message);
 
           }
         } else {
           listAsyncResult.cause().printStackTrace();
           message.fail(2, "find failed: " + listAsyncResult.cause().getMessage());
+        }
+      });
+
+    }
+
+    private void updateCardBalance(JsonObject user, Double newBalance, Message<JsonObject> message, Double availableCredit) {
+
+      JsonObject query = new JsonObject()
+        .put("username", user.getString("username"));
+
+      JsonObject update = new JsonObject().put("$set", new JsonObject()
+        .put("balance", newBalance.toString()));
+
+      client.updateCollection("card", query, update, res -> {
+        if (res.succeeded()) {
+          message.reply(newBalance.toString() + " " + availableCredit.toString());
+        } else {
+          message.fail(1, "update balance failed: " + res.cause().getMessage());
+        }
+
+      });
+
+    }
+    private void authenticateCharge(JsonObject user, AsyncResult<List<JsonObject>> listAsyncResult, Message<JsonObject> message) {
+
+      JsonObject authInfo = new JsonObject()
+        .put("username", user.getString("username"))
+        .put("password", user.getString("password"));
+
+      card_authentication.authenticate(authInfo, userAsyncResult -> {
+        if (userAsyncResult.succeeded()) {
+
+          JsonObject card = listAsyncResult.result().get(0);
+
+          Double newBalance = Double.parseDouble(card.getString("balance")) + user.getDouble("amount");
+
+          Double availableCredit = Double.parseDouble(card.getString("credit-limit")) - newBalance;
+
+          if (newBalance < Double.parseDouble(card.getString("credit-limit"))) {
+
+            updateCardBalance(user, newBalance, message, availableCredit);
+
+          } else {
+            message.fail(1,"Insufficient credit");
+          }
+
+        } else {
+          userAsyncResult.cause().printStackTrace();
+          message.fail(1, "charge failed: " + userAsyncResult.cause().getMessage());
         }
       });
 
@@ -223,45 +291,7 @@ public class Database extends AbstractVerticle {
         if (listAsyncResult.succeeded()) {
           if (!listAsyncResult.result().isEmpty()) {
 
-            JsonObject authInfo = new JsonObject()
-              .put("username", user.getString("username"))
-              .put("password", user.getString("password"));
-
-            card_authentication.authenticate(authInfo, userAsyncResult -> {
-              if (userAsyncResult.succeeded()) {
-
-                JsonObject card = listAsyncResult.result().get(0);
-
-                Double newBalance = Double.parseDouble(card.getString("balance")) + user.getDouble("amount");
-
-                Double availableCredit = Double.parseDouble(card.getString("credit-limit")) - newBalance;
-
-                if (newBalance < Double.parseDouble(card.getString("credit-limit"))) {
-
-                  JsonObject query = new JsonObject()
-                    .put("username", user.getString("username"));
-
-                  JsonObject update = new JsonObject().put("$set", new JsonObject()
-                    .put("balance", newBalance.toString()));
-
-                  client.updateCollection("card", query, update, res -> {
-                    if (res.succeeded()) {
-                      message.reply(newBalance.toString() + " " + availableCredit.toString());
-                    } else {
-                      message.fail(1, "update balance failed: " + res.cause().getMessage());
-                    }
-
-                  });
-
-                } else {
-                  message.fail(1,"Insufficient credit");
-                }
-
-              } else {
-                userAsyncResult.cause().printStackTrace();
-                message.fail(1, "charge failed: " + userAsyncResult.cause().getMessage());
-              }
-            });
+            authenticateCharge(user, listAsyncResult, message);
 
           } else {
             message.fail(1, "card not found");
--- final/src/main/java/com/backendtrack/final_/MainVerticle.java
+++ final/src/main/java/com/backendtrack/final_/MainVerticle.java
@@ -8,166 +8,62 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.JWTOptions;
 import io.vertx.ext.auth.KeyStoreOptions;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.JWTAuthHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
+  EventBus bussin;
+
+  JWTAuth jwtAuth;
+
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
 
-    JWTAuth jwtAuth = JWTAuth.create(vertx, new JWTAuthOptions()
+    jwtAuth = JWTAuth.create(vertx, new JWTAuthOptions()
       .setKeyStore(new KeyStoreOptions()
         .setType("jceks")
         .setPath("keystore.jceks")
         .setPassword("secret")));
 
     Verticle database = new Database();
-
     Verticle creditBureau = new CreditBureau();
 
     vertx.deployVerticle(creditBureau);
-
     vertx.deployVerticle(database);
 
-    EventBus bussin = vertx.eventBus();
+    bussin = vertx.eventBus();
 
     Router router = Router.router(vertx);
 
     router.route("/*").handler(BodyHandler.create());
 
-    router.post("/register").handler(routingContext -> {
+    router.post("/register").handler(this::handleRegister);
+    router.post("/login").handler(this::handleLogin);
 
-      JsonObject bus_Msg = new JsonObject()
-        .put("action", "register")
-        .put("user", routingContext.body().asJsonObject());
+    router.route("/actions/*").handler(JWTAuthHandler.create(jwtAuth));
 
-      bussin.request("database",bus_Msg, messageAsyncResult -> {
-        if (messageAsyncResult.succeeded()) {
-          routingContext.end("Thanks for Registering!");
-        } else {
-          routingContext.end("Registration failed! " + messageAsyncResult.cause());
-        }
-
-      });
-
-    });
-
-    router.post("/login").handler(routingContext -> {
-
-      JsonObject bus_Msg = new JsonObject()
-        .put("action", "login")
-        .put("user", routingContext.body().asJsonObject());
-
-      bussin.request("database",bus_Msg, messageAsyncResult -> {
-        if (messageAsyncResult.succeeded()) {
-
-          String token = jwtAuth.generateToken(
-            new JsonObject()
-              .put("username", routingContext.body().asJsonObject().getString("username")),
-            new JWTOptions().setIgnoreExpiration(true));
-
-          routingContext.end("Welcome Back! | token = " + token);
-        } else {
-          routingContext.end("Login failed! " + messageAsyncResult.cause());
-        }
-
-      });
-
-    });
-
-    //router.route("/actions/*").handler(JWTAuthHandler.create(jwtAuth));
-
-    router.post("/actions/charge").handler(routingContext -> {
-
-      JsonObject bus_Msg = new JsonObject()
-        .put("action", "charge")
-        .put("user", routingContext.body().asJsonObject());
-
-      bussin.request("database",bus_Msg, messageAsyncResult -> {
-        if (messageAsyncResult.succeeded()) {
-
-          String[] body = messageAsyncResult.result().body().toString().split(" ");
-
-          routingContext.end("Card has been charged. New Balance: " + body[0] + " available credit " + body[1]);
-        } else {
-          routingContext.end("Card Declined! " + messageAsyncResult.cause());
-        }
-
-      });
-
-    });
-
-    router.post("/actions/apply").handler(routingContext -> {
-
-      JsonObject bus_Msg = new JsonObject()
-        .put("action", "apply")
-        .put("user", routingContext.body().asJsonObject());
-
-      bussin.request("database",bus_Msg, messageAsyncResult -> {
-        if (messageAsyncResult.succeeded()) {
-
-          String[] card = messageAsyncResult.result().body().toString().split(" ");
-
-          routingContext.end("You have been approved for a credit limit of " + card[1] + " your card number is " + card[0]);
-        } else {
-          routingContext.end("Sorry we couldn't approve you today! " + messageAsyncResult.cause());
-        }
-
-      });
-
-    });
-
-    router.get("/actions/account").handler(routingContext -> {
-
-      routingContext.end("Account");
-
-    });
-
+    router.post("/actions/charge").handler(this::handleCharge);
+    router.post("/actions/apply").handler(this::handleApply);
+    router.get("/actions/account").handler(this::handleAccount);
 
     Route route_Users = router.route("/admin/users");
 
-    route_Users.method(HttpMethod.POST).handler(routingContext -> {
-
-      routingContext.end("Added User");
-
-    });
-
-    route_Users.method(HttpMethod.GET).handler(routingContext -> {
-
-      routingContext.end("User:__");
-
-    });
-
-    route_Users.method(HttpMethod.PUT).handler(routingContext -> {
-
-      routingContext.end("User Changed!");
-
-    });
+    route_Users.method(HttpMethod.POST).handler(this::handleAdminUserPost);
+    route_Users.method(HttpMethod.GET).handler(this::handleAdminUserGet);
+    route_Users.method(HttpMethod.PUT).handler(this::handleAdminUserPut);
 
     Route route_Cards = router.route("/admin/cards");
 
-    route_Cards.method(HttpMethod.POST).handler(routingContext -> {
-
-      routingContext.end("Card created!");
-
-    });
-
-    route_Cards.method(HttpMethod.GET).handler(routingContext -> {
-
-      routingContext.end("Card:___");
-
-    });
-
-    route_Cards.method(HttpMethod.PUT).handler(routingContext -> {
-
-      routingContext.end("Card changed!");
-
-    });
+    route_Cards.method(HttpMethod.POST).handler(this::handleAdminCardPost);
+    route_Cards.method(HttpMethod.GET).handler(this::handleAdminCardGet);
+    route_Cards.method(HttpMethod.PUT).handler(this::handleAdminCardPut);
 
     vertx.createHttpServer().requestHandler(router).listen(8888, http -> {
       if (http.succeeded()) {
@@ -178,4 +74,125 @@ public class MainVerticle extends AbstractVerticle {
       }
     });
   }
+
+  private void handleRegister(RoutingContext routingContext) {
+    JsonObject bus_Msg = new JsonObject()
+      .put("action", "register")
+      .put("user", routingContext.body().asJsonObject());
+
+    bussin.request("database",bus_Msg, messageAsyncResult -> {
+      if (messageAsyncResult.succeeded()) {
+        routingContext.end("Thanks for Registering!");
+      } else {
+        routingContext.end("Registration failed! " + messageAsyncResult.cause());
+      }
+
+    });
+  }
+
+  private void handleLogin(RoutingContext routingContext) {
+    JsonObject bus_Msg = new JsonObject()
+      .put("action", "login")
+      .put("user", routingContext.body().asJsonObject());
+
+    bussin.request("database",bus_Msg, messageAsyncResult -> {
+      if (messageAsyncResult.succeeded()) {
+
+        String token = jwtAuth.generateToken(
+          new JsonObject()
+            .put("username", routingContext.body().asJsonObject().getString("username")),
+          new JWTOptions().setIgnoreExpiration(true));
+
+        routingContext.end("Welcome Back! | token = " + token);
+      } else {
+        routingContext.end("Login failed! " + messageAsyncResult.cause());
+      }
+
+    });
+  }
+
+  private void handleCharge(RoutingContext routingContext) {
+
+    User jwtUser = routingContext.user();
+
+    JsonObject prince = jwtUser.principal();
+
+    String username = prince.getString("username");
+
+    JsonObject bus_Msg = new JsonObject()
+      .put("action", "charge")
+      .put("user", routingContext.body().asJsonObject()
+        .put("username", username)
+        .put("password", routingContext.body().asJsonObject().getString("card-number")));
+
+    bussin.request("database",bus_Msg, messageAsyncResult -> {
+      if (messageAsyncResult.succeeded()) {
+
+        String[] body = messageAsyncResult.result().body().toString().split(" ");
+
+        routingContext.end("Card has been charged. New Balance: " + body[0] + " available credit " + body[1]);
+      } else {
+        routingContext.end("Card Declined! " + messageAsyncResult.cause());
+      }
+
+    });
+  }
+
+  private void handleApply(RoutingContext routingContext) {
+
+    User jwtUser = routingContext.user();
+
+    JsonObject prince = jwtUser.principal();
+
+    String username = prince.getString("username");
+
+    JsonObject bus_Msg = new JsonObject()
+      .put("action", "apply")
+      .put("user", routingContext.body().asJsonObject()
+        .put("username", username));
+
+    bussin.request("database",bus_Msg, messageAsyncResult -> {
+      if (messageAsyncResult.succeeded()) {
+
+        String[] card = messageAsyncResult.result().body().toString().split(" ");
+
+        routingContext.end("You have been approved for a credit limit of " + card[1] + " your card number is " + card[0]);
+      } else {
+        routingContext.end("Sorry we couldn't approve you today! " + messageAsyncResult.cause());
+      }
+
+    });
+  }
+  private void handleAccount(RoutingContext routingContext) {
+
+    routingContext.end("Account");
+
+  }
+
+  private void handleAdminUserPost(RoutingContext routingContext) {
+
+    routingContext.end("Added User");
+
+  }
+
+  private void handleAdminUserGet(RoutingContext routingContext) {
+    routingContext.end("User:__");
+  }
+
+  private void handleAdminUserPut(RoutingContext routingContext) {
+    routingContext.end("User Changed!");
+  }
+
+  private void handleAdminCardPost(RoutingContext routingContext) {
+    routingContext.end("Card created!");
+  }
+
+  private void handleAdminCardGet(RoutingContext routingContext) {
+    routingContext.end("Card:___");
+  }
+
+  private void handleAdminCardPut(RoutingContext routingContext) {
+    routingContext.end("Card changed!");
+  }
+
 }
